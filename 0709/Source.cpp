42c204d73ab6f9e196553f046b2013c74016c9c3
My Name <kiykobogdan@gmail.com>
2022-09-07 09:17:45 +0300

    Добавлен код с прошлой пары.

---
+++ 0709/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class yxx {
+	int x;
+	int y;
+	int nwl;
+	int nwl2;
+public:
+
+	yxx() {
+
+	}
+	yxx(int a, int b) {
+		x = a;
+		y = b;
+	}
+};
+
+int main()
+{
+
+
+
+}
